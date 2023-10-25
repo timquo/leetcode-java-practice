72b2eec63e27ca0401eff80e4818328e4292a379
Dmitri Stalnuhhin <dmitri.stalnuhhin@gmail.com>
2023-10-25 23:14:04 +0300

    add solution for the problem #2235

---
+++ Problem2235.java
@@ -0,0 +1,12 @@
+public class Problem2235 {
+    public static void main(String[] args) {
+        /*
+            Given two integers num1 and num2, return the sum of the two integers.
+         */
+        System.out.println("For the given input solution is: " + solve(3, 5));
+    }
+
+    public static int solve(int num1, int num2) {
+        return num1 + num2;
+    }
+}
